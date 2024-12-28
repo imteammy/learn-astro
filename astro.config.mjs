cc4496ed2494a25be3f30fef1ac3e24fdcbba589
peerawat <peerawat@github.com>
2024-12-28 22:39:00 +0700

    vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,12 @@ import tailwind from '@astrojs/tailwind';
 
 import react from '@astrojs/react';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
-    integrations: [react(), mdx(), sitemap(), tailwind()],
+  // site: 'https://example.com',
+  integrations: [react(), mdx(), sitemap(), tailwind()],
+  adapter: vercel(),
+  output: 'server'
 });
