e2535dd5dbb08890b707bfdd0dc60c9b1fb5eac9
peerawat <peerawat@github.com>
2024-12-28 22:44:04 +0700

    netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,12 +7,15 @@ import tailwind from '@astrojs/tailwind';
 
 import react from '@astrojs/react';
 
-import vercel from '@astrojs/vercel';
+// import vercel from '@astrojs/vercel';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   // site: 'https://example.com',
   integrations: [react(), mdx(), sitemap(), tailwind()],
-  adapter: vercel(),
+  adapter: netlify({
+    edgeMiddleware: true
+  }),
   output: 'server'
 });
